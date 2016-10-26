e626831035a974ab00d8adbd784be760b443b754
Лада Жданова <kalakilada@outlook.com>
2016-10-26 12:43:38 +0300

    continue

--- app/app.js
+++ app/app.js
@@ -1,43 +1,57 @@
-var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.pagination','ui.grid.cellNav','ui.grid.pinning']);
+var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.cellNav', 'ui.grid.pinning']);
 
-app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http)  {
-  $scope.gridOptions2 = {
-    modifierKeysToMultiSelectCells: true,
-    showGridFooter: true,
-    enablePaginationControls: false,
-    paginationPageSize: 5,
-    columnDefs: [
-      {name:'id'},
-      { name: 'firstName' },
-      { name: 'lastName' },
-      { name: 'company' }
-    ]
-  };
+app.controller('MainCtrl', ['$scope', '$http', '$log', function ($scope, $http, $log) {
+    /* Table*/
+    $scope.gridOptions = {
+        modifierKeysToMultiSelectCells: true,
+        enablePaginationControls: false,
+        paginationPageSize: 5
+    };
 
-  /*$scope.currentFocused = "";
+    $scope.gridOptions.columnDefs = [
+        {name: 'id'},
+        {name: 'firstName'},
+        {name: 'lastName'},
+        {name: 'company'}
+    ];
 
-  $scope.getCurrentFocus = function(){
-    var rowCol = $scope.gridApi.cellNav.getFocusedCell();
-    if(rowCol !== null) {
-      $scope.currentFocused = 'Row Id:' + rowCol.row.entity.id + ' col:' + rowCol.col.colDef.name;
+        /*Focus*/
+    $scope.currentFocused = "";
+
+    $scope.getCurrentFocus = function () {
+        var rowCol = $scope.gridApi.cellNav.getFocusedCell();
+        if (rowCol !== null) {
+          //  $scope.currentFocused = 'Row Id:' + rowCol.row.entity.id + ' col:' + rowCol.col.columnDefs.name;
+            console.log('Row Id:' + rowCol.row.entity.id/*+' col:' + rowCol.col.columnDefs.name*/);
+        }
+    };
+
+
+        /*PaginationPageSize*/
+    $scope.go = function (items) {
+        $scope.gridOptions.paginationPageSize = items;
     }
-  };
-  */
-  $scope.go=function (items) {
-    $scope.gridOptions2.paginationPageSize=items;
-  }
 
-  
-  $scope.gridOptions2.onRegisterApi = function (gridApi) {
-    $scope.gridApi2 = gridApi;
-  }
-  $http.get('repository.json')
-      .success(function (data) {
-        $scope.gridOptions2.data = data;
-        console.log('success',result);
-      })
-      .error(function (result) {
-        console.log('error',result);
-      });
+
+    $scope.gridOptions.onRegisterApi = function (gridApi) {
+        $scope.gridApi = gridApi;
+    }
+
+    $http.get('repository.json')
+        .success(function (data) {
+            $scope.gridOptions.data = data;
+            console.log('success', result);
+        })
+        .error(function (result) {
+            console.log('error', result);
+        });
 
 }]);
+app.controller('StudentController', function ($scope) {
+    $scope.master = {};
+    console.log("proverka");
+    $scope.save = function (user) {
+        $scope.master = angular.copy(user);
+        console.log($scope.master);
+    };
+});
