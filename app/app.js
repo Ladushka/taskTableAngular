888829b07be906ed2e250654f28dffc3d65945c0
Лада Жданова <kalakilada@outlook.com>
2016-10-27 12:45:19 +0300

    service

--- app/app.js
+++ app/app.js
@@ -1,12 +1,29 @@
-var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.cellNav', 'ui.grid.pinning']);
-var dataBuf={};
 
-app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http,dataCompanyService) {
+var dataBuf={};
+var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.cellNav', 'ui.grid.pinning'])
+.service('dataCompanyService',  function(){
+    this.name = {
+        first: 'Alice',
+        last: 'Green'
+    };
+
+
+    /* var companies= [ "Exadel", "Epam", "MainSoft", "InnovationGroup", "Belvest", "Anderson"];
+     return {
+     companies: function() {
+     return companies;
+     }
+     */
+})
+.controller('MainCtrl', ['$scope', '$http','dataCompanyService', function ($scope, $http,dataCompanyService) {
     /* Table*/
     $scope.currentFocused = "";
     //$scope.rowCol;
-    
-    //$scope.company=dataCompanyService.companies;
+    /*$scope.dataCompanyService=dataCompanyService;
+    console.log($scope.dataCompanyService);*/
+   // $scope.name = dataCompanyService.modifierKeysToMultiSelectCells;
+    //console.log($scope.name);
+
     
     $scope.gridOptions = {
         modifierKeysToMultiSelectCells: true,
@@ -49,12 +66,12 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http,dataCompa
         /*PaginationPageSize*/
     $scope.go = function (items) {
         $scope.gridOptions.paginationPageSize = items;
-    }
+    };
 
 
     $scope.gridOptions.onRegisterApi = function (gridApi) {
         $scope.gridApi = gridApi;
-    }
+    };
 
     $scope.master = {};
 
@@ -62,12 +79,9 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http,dataCompa
             $scope.user={};
             $('#myModal').modal('hide')
     };
-
+        ///Развлекаловка с сервисами
+    $scope.name = dataCompanyService.name;
+    console.log($scope.name.first );
 }]);
 
-app.factory('dataCompanyService', function(){
-    return{
-        companies: [ "Exadel", "Epam", "MainSoft", "InnovationGroup", "Belvest", "Anderson"]
 
-    };
-});
