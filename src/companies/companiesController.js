ab39fa00e3de2f3f09de859acd5f45796c30f810
Lada Zhdanova <Sherlock8>
2016-11-02 18:29:03 +0300

    small changes

--- src/companies/companiesController.js
+++ src/companies/companiesController.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module('app.companies',[])
+    angular.module('app.companies', [])
 
         .controller('companiesController', ['$scope', '$http', '$uibModal', '$document', 'companiesService',
             function ($scope, $http, $uibModal, $document, companiesService) {
