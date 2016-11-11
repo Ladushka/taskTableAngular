ed1f801a8c843cc2f9be3964f09357002888cb85
Lada Zhdanova <Sherlock8>
2016-11-11 15:44:28 +0300

    tabs with ui bootstrap in projects component

--- src/projects/projectsController.js
+++ src/projects/projectsController.js
@@ -1,23 +1,13 @@
 (function () {
     'use strict';
-    
-    angular.module('app.projects',[])
-        .controller('projectsController',function ($scope,projectsService) {
-            $scope.tab = 1;
 
-            $scope.setTab = function(newValue){
-                this.tab = newValue;
-            };
-
-            $scope.isSet = function(tabName){
-                return this.tab === tabName;
-            };
+    angular.module('app.projects', [])
+        .controller('projectsController', function ($scope, projectsService) {
 
             projectsService.getProjects().then(function (response) {
                 $scope.project = (response.data).filter(function (item) {
                     return item.id == projectsService.getId();
                 })[0];
-                console.log($scope.project);
             });
 
 
