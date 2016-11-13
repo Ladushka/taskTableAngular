5a082a09886c21e861d0708bda5e268b5a4297d3
Лада Жданова <kalakilada@outlook.com>
2016-11-14 00:17:33 +0300

    made header as component list of companies made as a table

--- src/companies/companiesController.js
+++ src/companies/companiesController.js
@@ -2,14 +2,31 @@
     'use strict';
 
     angular.module('app.companies', [])
-        .controller('companiesController', function ($scope, $routeParams, companiesService, projectsService) {
-            companiesService.getCompanies().then(function (response) {
-                $scope.companies = response.data;
+        .controller('companiesController', function ($scope, $routeParams, companiesService) {
 
+            $scope.currentPage = 1;
+            $scope.itemsPerPage = 5;
+
+            $scope.pageChanged = function () {
+                $scope.gridApi.pagination.seek($scope.currentPage);
+            };
+
+            $scope.gridOptions = companiesService.gridOptions().gridOptions;
+            $scope.gridOptions.columnDefs = companiesService.gridOptions().columnDefs;
+
+            $scope.gridOptions.onRegisterApi = function (gridApi) {
+                $scope.gridApi = gridApi;
+            };
+
+            companiesService.getCompanies().then(function (response) {
+                $scope.gridOptions.data = response.data;
                 $scope.company = (response.data).filter(function (item) {
-                    return item.id == $routeParams.companyID;
+                    return item.company.id == $routeParams.companyID;
                 })[0];
+                $scope.totalItems = response.data.length;
             });
-            projectsService.sendId($routeParams.companyID);
+
+
+            // projectsService.sendId($routeParams.companyID);
         });
 })();
--- src/companies/companiesService.js
+++ src/companies/companiesService.js
@@ -2,10 +2,24 @@
     'use strict';
 
     angular.module('app.companies')
-        .service('companiesService',function ($http) {
+        .service('companiesService', function ($http) {
             return {
                 getCompanies: function () {
                     return $http.get('src/globals/json/companies.json');
+                },
+                gridOptions: function () {
+                    return {
+                        gridOptions: {
+                            enablePaginationControls: false,
+                            enableRowSelection: true,
+                            selectionRowHeaderWidth: 35,
+                            rowHeight: 35,
+                            paginationPageSize: 5
+                        },
+                        columnDefs: [
+                            {name: 'company', cellTemplate: 'src/companies/href.html'}
+                        ]
+                    };
                 }
             };
         });
