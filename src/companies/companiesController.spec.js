6f757899c17d464fb1fc30e9aaf419da246f8334
Lada Zhdanova <Sherlock8>
2016-12-01 17:30:40 +0300

    checking unit test

---
+++ src/companies/companiesController.spec.js
@@ -0,0 +1,11 @@
+describe('Controllers', function () {
+    'use strict';
+    
+    beforeEach(function () {
+        module('app.companies');
+    });
+
+    it('contains spec with an expectation', function() {
+        expect(true).toBe(true);
+    });
+});
