0b48d5eb3e81b0df3dbd7c4bc59d091b646a4880
Lada Zhdanova <Sherlock8>
2016-12-05 18:05:41 +0300

    test for unit-test problem with $uibModal

--- karma.conf.js
+++ karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function (config) {
             'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
             'bower_components/angular-touch/angular-touch.js',
             'bower_components/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js',
+            'bower_components/angular-resource/angular-resource.js',
 
             'tests/**/*.js',
             'src/**/*.js'
---
+++ tests/controllers/companiesControllerTest.spec.js
@@ -0,0 +1,30 @@
+describe("controllers",function () {
+    beforeEach(module('app.companies'));
+
+        describe('companiesController', function () {
+            var ctrl, $scope;
+            beforeEach(inject(function ($rootScope, $uibModal, $controller, companiesService) {
+                $scope = $rootScope.$new();
+                $scope.carousel = {
+                    next: function () {
+                    }
+                };
+                ctrl = $controller('companiesController', {
+                    $scope: $scope,
+                    $uibModal: $uibModal,
+                    $routeParams: {companyID:'4'},
+                    companiesService: companiesService
+                });
+               spyOn(companiesService, 'getCountries');
+
+               //$scope.$digest();
+            }));
+
+            it('should start history tracking', inject(function () {
+                expect(true).toBe(true);
+            }));
+        });
+});
+
+
+
