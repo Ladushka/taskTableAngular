8469ce33b60c21511370c2dd17ab18dd592aaed0
Ladushka <kalaki@rambler.ru>
2016-12-26 01:19:23 +0300

    e2e-tests for projects page

--- e2e-tests/tests.js
+++ e2e-tests/tests.js
@@ -39,8 +39,41 @@ describe('my app', function () {
         it('should be open modal add', function () {
             var buttons = element.all(by.css('button'));
             buttons.get(1).click();
-            var lines = element.all(by.css('input'));
-            expect(lines.count()).toEqual(7);
+
+        });
+
+    });
+
+    describe('Projects Page', function () {
+        var ptor;
+        beforeEach(function () {
+            browser.get("/#/about/4");
+        });
+
+        it('should be navigate to companies list', function () {
+            var navBar = element.all(by.className('navbar-brand'));
+            navBar.get(1).click();
+            expect(browser.getCurrentUrl()).toContain('/about');
+        });
+
+        it('should be open modal add', function () {
+
+            var buttons = element.all(by.css('button'));
+            expect(buttons.count()).toEqual(3);
+
+            var companyNameValid = element(by.binding('company.company.name'));
+            expect(companyNameValid.getText()).toContain('InnovationGroup');
+            expect(element(by.exactBinding('company.address.country')).isPresent()).toBe(true);
+
+            buttons.get(2).click();
+            var input = element(by.model('companyEdit.company.name'));
+            input.sendKeys('123');
+            expect(input.getAttribute('value')).toBe('InnovationGroup123');
+
+            expect(buttons.count()).toEqual(4);
+            buttons.get(0).click();
+            expect(input.getAttribute('value')).toBe('InnovationGroup');
+
         });
 
     });
